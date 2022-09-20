d6a36eef1428b9f27538dab5ed95a6590efbef9e
Bhupender Soni <official.soni532@gmail.com>
2022-09-20 18:50:27 +0530

    modified prime code for intentional conflit

--- IsPrime.js
+++ IsPrime.js
@@ -1,2 +1,8 @@
-let N = 10;
-//check prime from 1 to N
+function isPrime(N) {
+    for (let i = 2; i <= N ** 0.5; i++) 
+        if (N % i == 0)
+            return false;
+    return true;
+}
+let input = 13;
+console.log(isPrime(input))
